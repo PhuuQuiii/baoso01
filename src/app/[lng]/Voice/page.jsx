8a9db5b7933a22db4cf9f264694b60d33c9e295b
Trần Tín <ttins2004@gmail.com>
2024-03-03 00:34:26 +0700

    ws

--- src/app/[lng]/Voice/page.jsx
+++ src/app/[lng]/Voice/page.jsx
@@ -1,3 +1,4 @@
+
 "use client";
 import React, { useState, useEffect } from "react";
 
@@ -5,9 +6,41 @@ const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
-const Page = () => {
+const Page = ({ params: { lng } }) => {
+  const [canListening, setCanListening] = useState(true);
   const [isListening, setIsListening] = useState(false);
   const [spokenText, setSpokenText] = useState("");
+  const [answerText, setAnswerText] = useState("");
+  const [socket, setSocket] = useState(null); // State for WebSocket
+
+  if (!canListening){
+    var url= "/vi/"; 
+    window.location = url; 
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    // Establish WebSocket connection
+    setSocket(ws);
+  
+    ws.onopen = () => {
+      console.log("WebSocket connection opened");
+    };
+  
+    ws.onclose = (event) => {
+      console.log("WebSocket connection closed", event);
+    };
+  
+    ws.onerror = (error) => {
+      console.error("WebSocket error", error);
+    };
+  
+    return () => {
+      if (ws.readyState !== ws.CLOSED) {
+        ws.close();
+      }
+    };
+  }, []);
 
   const toggleListening = () => {
     setIsListening((prevState) => !prevState);
@@ -22,13 +55,33 @@ const Page = () => {
       const current = event.resultIndex;
       const transcript = event.results[current][0].transcript;
       setSpokenText(transcript);
+      console.log(socket.readyState)
+      
+      if (socket) {
+        socket.send(JSON.stringify({ transcript, lng }));
+      } else {
+        console.error("WebSocket connection is not open");
+      }
     };
 
     recognition.onend = () => {
       console.log("Voice recognition ended");
       setIsListening(false);
     };
-  }, []);
+  }, [socket]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        var answerMessage = data.message
+        if ((answerMessage === "Tạm biệt quý khách hàng.") || (answerMessage === "Goodbye to our valued customers.")){
+          setCanListening(false)
+        }
+        setAnswerText("\"" + answerMessage + "\"")
+      };
+    }
+  }, [socket]);
 
   useEffect(() => {
     if (isListening) {
@@ -41,6 +94,7 @@ const Page = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <p className="text-4xl font-bold mb-8">{spokenText}</p>
+      <p className="text-4xl font-bold mb-8">{answerText}</p>
       <button
         onClick={toggleListening}
         className={`rounded-full p-4 mb-12 ${
