05111dd9d8edc2201a7661e6954368827b50712d
Nguyenybin2015 <nguyenybin2015@gmail.com>
2024-03-04 01:44:20 +0700

    add  map

--- src/app/[lng]/Map/page.jsx
+++ src/app/[lng]/Map/page.jsx
@@ -4,8 +4,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Map
+    <div className="flex items-center justify-center h-screen">
+      <iframe
+        className="w-full h-full"
+        src="/index.html"
+        title="Fullscreen HTML"
+      />
     </div>
   )
 }
