1bd40c96c36acec6251c2d371d67e33b64cfc79d
PhuuQuiii <nguyenphuqui170304@gmail.com>
2024-03-05 13:13:05 +0700

    updtae ảnh logo và chỉnh sửa giao diện

--- src/app/[lng]/page.js
+++ src/app/[lng]/page.js
@@ -3,40 +3,55 @@ import Link from "next/link";
 export default function Page({ params: { lng } }) {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-screen">
-        <h1 className="text-center text-8xl mb-16">XIN CHÀO</h1>
-        <h1 className="text-center text-8xl mb-16">WELCOME</h1>
-        <p className="text-center text-4xl mb-4">VUI LÒNG LỰA CHỌN NGÔN NGỮ</p>
-        <p className="text-center text-4xl mb-16">
-          PLEASE SELECT YOUR LANGUAGE
-        </p>
-        <div className="grid grid-cols-3 gap-4 w-2/3">
-          <Link href={`/${lng === 'en' ? 'vi' : 'en'}/Home`}>
-            <button className="bg-blue-500 text-white text-4xl py-4 px-6 w-full h-40 rounded-lg">
-              {lng === 'en' ? 'TIẾNG VIỆT' : 'ENGLISH'}
-            </button>
-          </Link>
-          <div className="invisible">02</div>
-          <Link href={`/${lng}/Home`}>
-            <button className="bg-blue-500 text-white text-4xl py-4 px-6 w-full h-40 rounded-lg">
-              {lng === 'en' ? 'ENGLISH' : 'TIẾNG VIỆT'}
-            </button>
-          </Link>
+      <div className=" flex flex-col justify-center">
+        <div className=" flex justify-between w-full ">
+          <div className="flex flex-col justify-center items-center p-4">
+            <img
+              src="/VAA.jpg"
+              alt="Học viện Hàng không Việt Nam"
+              className=" w-28"
+            />
+            <p className="text-center text-2xl font-semibold mt-4">
+              HỌC VIỆN HÀNG KHÔNG VIỆT NAM
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center p-4">
+            <img
+              src="/Color_Logo đơn.png"
+              alt="Khoa Công nghệ thông tin"
+              className=" w-32"
+            />
+            <p className="text-center text-2xl font-semibold mt-4">
+              KHOA CÔNG NGHỆ THÔNG TIN
+            </p>
+          </div>
+        </div>
+        <div className=" mt-12 flex flex-col justify-center">
+          <h1 className="text-center text-8xl mb-16">XIN CHÀO</h1>
+          <h1 className="text-center text-8xl mb-16">WELCOME</h1>
+          <p className="text-center text-4xl mb-4">
+            VUI LÒNG LỰA CHỌN NGÔN NGỮ
+          </p>
+          <p className="text-center text-4xl mb-16">
+            PLEASE SELECT YOUR LANGUAGE
+          </p>
+          <div className=" w-1/2 mx-auto mt-12">
+            <div className=" flex justify-between w-full">
+              <Link href={`/${lng === "en" ? "vi" : "en"}/Home`}>
+                <button className="bg-blue-500 text-white text-4xl py-10 px-24 rounded-lg">
+                  {lng === "en" ? "TIẾNG VIỆT" : "ENGLISH"}
+                </button>
+              </Link>
+              <div className="invisible">02</div>
+              <Link href={`/${lng}/Home`}>
+                <button className="bg-blue-500 text-white text-4xl py-10 px-24  rounded-lg">
+                  {lng === "en" ? "ENGLISH" : "TIẾNG VIỆT"}
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
 }
-
-// import Link from 'next/link'
-
-// export default function Page({ params: { lng } }) {
-//   return (
-//     <>
-//       <h1>Hi there!</h1>
-//       <Link href={`/${lng}/Home`}>
-//         Home
-//       </Link>
-//     </>
-//   )
-// }
