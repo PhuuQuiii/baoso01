d42641dfb9d36684ed23066b88bd77673e783fc4
PhuuQuiii <nguyenphuqui170304@gmail.com>
2024-03-05 13:41:10 +0700

    chỉnh sửa lại page

--- src/app/[lng]/page.js
+++ src/app/[lng]/page.js
@@ -4,10 +4,10 @@ export default function Page({ params: { lng } }) {
   return (
     <>
       <div className=" flex flex-col justify-center">
-        <div className=" flex justify-between w-full ">
+        <div className=" flex justify-between w-[80%] mx-auto mt-6">
           <div className="flex flex-col justify-center items-center p-4">
             <img
-              src="/VAA.jpg"
+              src="/VAA.png"
               alt="Học viện Hàng không Việt Nam"
               className=" w-28"
             />
@@ -27,8 +27,12 @@ export default function Page({ params: { lng } }) {
           </div>
         </div>
         <div className=" mt-12 flex flex-col justify-center">
-          <h1 className="text-center text-8xl mb-16">XIN CHÀO</h1>
-          <h1 className="text-center text-8xl mb-16">WELCOME</h1>
+          <h1 className="text-center text-7xl font-bold text-gray-800 mb-16">
+            XIN CHÀO
+          </h1>
+          <h1 className="text-center text-7xl font-bold text-gray-800 mb-16">
+            WELCOME
+          </h1>
           <p className="text-center text-4xl mb-4">
             VUI LÒNG LỰA CHỌN NGÔN NGỮ
           </p>
