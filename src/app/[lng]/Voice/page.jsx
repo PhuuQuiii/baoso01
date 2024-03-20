34e0ed35be5e55ac1c3fc7783782759d6b01c4cf
PhuuQuiii <nguyenphuqui170304@gmail.com>
2024-03-20 13:20:34 +0700

    xét voice qua map liền

--- src/app/[lng]/Voice/page.jsx
+++ src/app/[lng]/Voice/page.jsx
@@ -1,4 +1,3 @@
-
 "use client";
 import React, { useState, useEffect } from "react";
 
@@ -15,19 +14,21 @@ const Page = ({ params: { lng } }) => {
   const [answerText, setAnswerText] = useState("");
   const [socket, setSocket] = useState(null); // State for WebSocket
 
+  //mac dinh chuyen qua map
+  var url = "/" + lng + "/Map";
+  window.location = url;
+
   if (screenLocation === 1) {
     var url = "/vi/";
     window.location = url;
-  }
-  else if (screenLocation === 2) {
-    var url = "/" + lng +"/Lookup";
+  } else if (screenLocation === 2) {
+    var url = "/" + lng + "/Lookup";
+    window.location = url;
+  } else if (screenLocation === 3) {
+    var url = "/" + lng + "/Map";
     window.location = url;
   }
-  else if (screenLocation === 3) {
-    var url = "/" + lng +"/Map";
-    window.location = url;
-  }
-  
+
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080");
     // Establish WebSocket connection
@@ -65,11 +66,11 @@ const Page = ({ params: { lng } }) => {
       const current = event.resultIndex;
       const transcript = event.results[current][0].transcript;
       setSpokenText(transcript);
-      console.log(socket.readyState)
+      console.log(socket.readyState);
 
       if (socket) {
         socket.send(JSON.stringify({ transcript, lng }));
-        console.log({ transcript, lng })
+        console.log({ transcript, lng });
       } else {
         console.error("WebSocket connection is not open");
       }
@@ -85,34 +86,39 @@ const Page = ({ params: { lng } }) => {
     if (socket) {
       socket.onmessage = (event) => {
         const data = JSON.parse(event.data);
-        var answerMessage = data.message[0]
-        var language = data.message[1]
+        var answerMessage = data.message[0];
+        var language = data.message[1];
         if (language === "vi") speakAnswer.lang = "vi-VN";
         else if (language === "en") speakAnswer.lang = "en-US";
         speakAnswer.text = answerMessage;
-        speechSynthesis.speak(speakAnswer); 
+        speechSynthesis.speak(speakAnswer);
 
-
-        if ((answerMessage === "Tạm biệt quý khách hàng.") || (answerMessage === "Goodbye to our valued customers.")){
-          setscreenLocation(1)
-        }
-        else if (answerMessage.includes("là danh sách chuyến bay hiện có") || answerMessage.includes("list of available flights.")){
-          setscreenLocation(2)
-        }
-        else if (answerMessage.includes("hiện trên bản đồ.") || answerMessage.includes("shown on the map.")){
-          setscreenLocation(3)
+        if (
+          answerMessage === "Tạm biệt quý khách hàng." ||
+          answerMessage === "Goodbye to our valued customers."
+        ) {
+          setscreenLocation(1);
+        } else if (
+          answerMessage.includes("là danh sách chuyến bay hiện có") ||
+          answerMessage.includes("list of available flights.")
+        ) {
+          setscreenLocation(2);
+        } else if (
+          answerMessage.includes("hiện trên bản đồ.") ||
+          answerMessage.includes("So on the map.")
+        ) {
+          setscreenLocation(3);
         }
         // const utterance = new SpeechSynthesisUtterance(answerMessage);
 
         // const isVietnamese = /[\p{Script=Latin} \p{Mark}]/u.test(answerText);
         // utterance.lang = 'vi-VN';
-        
+
         // window.speechSynthesis.speak(utterance);
-        setAnswerText("\"" + answerMessage + "\"")
+        setAnswerText('"' + answerMessage + '"');
       };
     }
   }, [socket]);
-
 
   useEffect(() => {
     if (isListening) {
@@ -128,8 +134,9 @@ const Page = ({ params: { lng } }) => {
       <p className="text-4xl font-bold mb-8">{answerText}</p>
       <button
         onClick={toggleListening}
-        className={`rounded-full p-4 mb-12 ${isListening ? "bg-blue-500" : "bg-white"
-          }`}
+        className={`rounded-full p-4 mb-12 ${
+          isListening ? "bg-blue-500" : "bg-white"
+        }`}
         style={{ marginTop: "calc(100vh / 3)" }}
       >
         <img
