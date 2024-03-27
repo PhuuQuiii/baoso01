dee62035db30041c8c19c89e0a22e7d2bfe51167
PhuuQuiii <nguyenphuqui170304@gmail.com>
2024-03-27 12:44:08 +0700

    fix map

--- src/app/[lng]/Map/MapG/page.jsx
+++ src/app/[lng]/Map/MapG/page.jsx
@@ -7,66 +7,47 @@ import { Toaster, toast } from "sonner";
 import html from "./html";
 const page = (context) => {
   console.log(context.searchParams.id);
-  
+
   useEffect(() => {
     if (context.searchParams.id === "ICONATM") {
-      IATM()
-    }
-    else if (context.searchParams.id === "ITolet-TN1"){
-      ITolet_TN1()
-    }
-    else if (context.searchParams.id === "ITolet-TN2"){
-      ITolet_TN2()
-    }
-    else if (context.searchParams.id === "ICONSCC"){
-      ISCC()
-    }
-    else if (context.searchParams.id === "ILuggageTN1"){
-      ILuggage_TN()
-    }
-    else if (context.searchParams.id === "ILuggageTN2()"){
-      ILuggage_TN()
-    }
-    else if (context.searchParams.id === "ICONLuggageTN"){
-      ILuggage_TN()
-    }
-    else if (context.searchParams.id === "IRestaurant1"){
-      IRestaurant()
-    }
-    else if (context.searchParams.id === "ICONIDRestaurant1"){
-      IRestaurant()
-    }
-    else if (context.searchParams.id === "ICONIRestaurant1"){
-      IRestaurant()
-    }
-    else if (context.searchParams.id === "IStair1"){
-      IStair1()
-    }
-    else if (context.searchParams.id === "ICONIStair1"){
-      IStair1()
-    }
-    else if (context.searchParams.id === "IStair2"){
-      IStair2()
-    }
-    else if (context.searchParams.id === "ICONIStair2"){
-      IStair2()
-    }
-    else if (context.searchParams.id === "IElevatorSC"){
-      IElevatorSC()
-    }
-    else if (context.searchParams.id === "ICONIElevatorSC"){
-      IElevatorSC()
-    }
-    else if (context.searchParams.id === "Exit1"){
-      Exit_1()
-    }
-    else if (context.searchParams.id === "Exit2"){
-      Exit_2()
+      path.IATM();
+    } else if (context.searchParams.id === "ITolet-TN1") {
+      path.ITolet_TN1();
+    } else if (context.searchParams.id === "ITolet-TN2") {
+      path.ITolet_TN2();
+    } else if (context.searchParams.id === "ICONSCC") {
+      path.ISCC();
+    } else if (context.searchParams.id === "ILuggageTN1") {
+      path.ILuggage_TN();
+    } else if (context.searchParams.id === "ILuggageTN2") {
+      path.ILuggage_TN();
+    } else if (context.searchParams.id === "ICONLuggageTN") {
+      path.ILuggage_TN();
+    } else if (context.searchParams.id === "IRestaurant1") {
+      path.IRestaurant();
+    } else if (context.searchParams.id === "ICONIDRestaurant1") {
+      path.IRestaurant();
+    } else if (context.searchParams.id === "ICONIRestaurant1") {
+      path.IRestaurant();
+    } else if (context.searchParams.id === "IStair1") {
+      path.IStair1();
+    } else if (context.searchParams.id === "ICONIStair1") {
+      path.IStair1();
+    } else if (context.searchParams.id === "IStair2") {
+      path.IStair2();
+    } else if (context.searchParams.id === "ICONIStair2") {
+      path.IStair2();
+    } else if (context.searchParams.id === "IElevatorSC") {
+      path.IElevatorSC();
+    } else if (context.searchParams.id === "ICONIElevatorSC") {
+      path.IElevatorSC();
+    } else if (context.searchParams.id === "Exit1") {
+      path.Exit_1();
+    } else if (context.searchParams.id === "Exit2") {
+      path.Exit_2();
     }
   });
-  return (
-    html()
-  );
+  return html();
 };
 
 export default page;
